4de80074d51700b547040154aa4d1e8fab311f8b
jalex8188 <jalex8188@gmail.com>
2022-08-13 09:19:02 -0600

    working multi sensor

---
+++ AnalogInOutSerial_Multi/AnalogInOutSerial_Multi.ino
@@ -0,0 +1,81 @@
+const int numReadings = 10;
+const int threshold = 1000;   // an arbitrary threshold level that's in the range of the analog input
+const int thresholdLow = 800;   // an arbitrary threshold level that's in the range of the analog input
+const int ledPin = 13;       // pin that the LED is attached to
+
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+int activated_array[6] = {0, 0, 0, 0, 0, 0}; 
+int activated_sum = 0;                
+int activated = 0;
+
+const int analogInPin_0 = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPin_1 = A1;  // Analog input pin that the potentiometer is attached to
+const int analogInPin_2 = A2;  // Analog input pin that the potentiometer is attached to
+const int analogInPin_3 = A3;  // Analog input pin that the potentiometer is attached to
+const int analogInPin_4 = A4;  // Analog input pin that the potentiometer is attached to
+const int analogInPin_5 = A5;  // Analog input pin that the potentiometer is attached to
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  activated_sum = 0;
+  read_sensor(analogInPin_0, 0);
+  read_sensor(analogInPin_1, 1);
+  read_sensor(analogInPin_2, 2);
+  read_sensor(analogInPin_3, 3);
+  read_sensor(analogInPin_4, 4);
+  read_sensor(analogInPin_5, 5);
+
+  int i;
+  for (i=0; i< 6; i++) {
+      activated_sum += activated_array[i];
+  }
+
+  if (activated_sum > 0) {
+    if (activated == 0) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("ACTIVATED");
+      activated = 1;
+      }
+    }
+
+  else {
+    if (activated == 1) {
+      digitalWrite(ledPin, LOW);
+      Serial.println("DEACTIVATED");
+      activated = 0;
+    }
+  }
+
+  delay(100);
+  }
+
+void read_sensor(int analogInPin, int activated_array_num) {
+    sensorValue = analogRead(analogInPin);
+//    Serial.print("sensorValue ");
+//    Serial.print(activated_array_num);
+//    Serial.print(":");
+//    Serial.println(sensorValue);
+    
+    if (sensorValue > threshold) {
+      if (activated_array[activated_array_num] == 0) {
+        Serial.print("PAD_ACTIVATED ");
+        Serial.println(activated_array_num);
+        (activated_array[activated_array_num]) = 1;
+      }
+    }
+
+    if (sensorValue < thresholdLow){
+      if (activated_array[activated_array_num] == 1) {
+        Serial.print("PAD_DEACTIVATED ");
+        Serial.println(activated_array_num);
+        (activated_array[activated_array_num]) = 0;
+      }
+    }
+  }
